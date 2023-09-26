cb3c4499f38befa1c300a130b3723a1994ce15ae
YR <darkbuq@gmail.com>
2023-09-26 09:53:13 +0800

    add new

---
+++ byYR_DataGridView/byYR_dgv.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace byYR_DataGridView
+{
+    public class byYR_dgv
+    {
+        public void dgv_initialize(DataGridView dgv)
+        {
+            dgv.AllowUserToAddRows = false;
+
+            dgv.RowTemplate.Height = 25;
+            dgv.RowHeadersWidth = 10;
+
+            //dgv.Columns[1].DefaultCellStyle.BackColor = Color.LightYellow;
+            dgv.ColumnHeadersDefaultCellStyle.BackColor = Color.LightYellow;
+            dgv.EnableHeadersVisualStyles = false;
+
+            foreach (DataGridViewColumn column in dgv.Columns) //禁用排序
+            {
+                column.SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+
+            dgv.AutoGenerateColumns = false;
+        }
+
+
+        public void dgv_add_col(System.Windows.Forms.DataGridView dgv, string[] colname, string[] coltxt, string[] coltype, int[] colW)
+        {
+            for (int i = 0; i < colname.Length; i++)
+            {
+                if (coltype[i] == "txt")
+                {
+                    DataGridViewColumn col = new DataGridViewTextBoxColumn();
+                    col.DataPropertyName = colname[i];
+                    col.HeaderText = coltxt[i];
+                    col.Name = colname[i];
+                    col.Width = colW[i];
+
+                    dgv.Columns.Add(col);
+
+
+                }
+                else if (coltype[i] == "chk")
+                {
+                    DataGridViewColumn col = new DataGridViewCheckBoxColumn();
+                    col.DataPropertyName = colname[i];
+                    col.HeaderText = coltxt[i];
+                    col.Name = colname[i];
+                    col.Width = colW[i];
+
+                    dgv.Columns.Add(col);
+                }
+                else if (coltype[i] == "btn")
+                {
+                    DataGridViewButtonColumn col = new DataGridViewButtonColumn();
+                    col.DataPropertyName = colname[i];
+                    col.HeaderText = coltxt[i];
+                    col.UseColumnTextForButtonValue = true;
+                    col.Text = coltxt[i];
+                    col.Name = colname[i];
+                    col.Width = colW[i];
+
+                    dgv.Columns.Add(col);
+                }
+            }
+        }
+    }
+}
