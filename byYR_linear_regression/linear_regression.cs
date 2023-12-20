6d942cc71dbe0cf1343d1101525b837b5fc282cb
YR <darkbuq@gmail.com>
2023-12-20 09:39:51 +0800

    20231220

---
+++ byYR_linear_regression/linear_regression.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MathNet.Numerics;
+
+namespace byYR_linear_regression
+{
+    public class linear_regression
+    {
+        /// <summary>
+        /// double[] xdata = new double[] { -5, 25, 75 };
+        /// double[] ydata = new double[] { 9162.2049, 10471.28548, 6591.738952 };
+        /// use => Fit.Polynomial(xdata, ydata, degree)
+        /// return array is from low coefficient to high coefficient..
+        /// </summary>
+        /// <param name="xdata"></param>
+        /// <param name="ydata"></param>
+        /// <param name="degree"></param>
+        /// <returns></returns>
+        public double[] regression(double[] xdata, double[] ydata, int degree)
+        {
+            return Fit.Polynomial(xdata, ydata, degree);
+        }
+    }
+}
