771c941f0f0d67107c49e93dae7294d12becb7c0
YR <darkbuq@gmail.com>
2024-01-22 11:19:11 +0800

    20240122

---
+++ zz_SerialPort/SerialPort_01/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace SerialPort_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btn_conn = new System.Windows.Forms.Button();
+            this.txt_result = new System.Windows.Forms.TextBox();
+            this.btn_check = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(67, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(58, 27);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "COM1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "COM?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(131, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "BaudRate";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(209, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(69, 27);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.Text = "115200";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 19);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "DataBits";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(83, 65);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(26, 27);
+            this.textBox3.TabIndex = 0;
+            this.textBox3.Text = "8";
+            // 
+            // btn_conn
+            // 
+            this.btn_conn.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_conn.Location = new System.Drawing.Point(115, 65);
+            this.btn_conn.Name = "btn_conn";
+            this.btn_conn.Size = new System.Drawing.Size(69, 27);
+            this.btn_conn.TabIndex = 2;
+            this.btn_conn.Text = "conn";
+            this.btn_conn.UseVisualStyleBackColor = true;
+            this.btn_conn.Click += new System.EventHandler(this.btn_conn_Click);
+            // 
+            // txt_result
+            // 
+            this.txt_result.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_result.Location = new System.Drawing.Point(16, 125);
+            this.txt_result.Multiline = true;
+            this.txt_result.Name = "txt_result";
+            this.txt_result.Size = new System.Drawing.Size(262, 143);
+            this.txt_result.TabIndex = 0;
+            // 
+            // btn_check
+            // 
+            this.btn_check.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_check.Location = new System.Drawing.Point(209, 65);
+            this.btn_check.Name = "btn_check";
+            this.btn_check.Size = new System.Drawing.Size(69, 27);
+            this.btn_check.TabIndex = 2;
+            this.btn_check.Text = "*IDN?";
+            this.btn_check.UseVisualStyleBackColor = true;
+            this.btn_check.Click += new System.EventHandler(this.btn_check_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 288);
+            this.Controls.Add(this.btn_check);
+            this.Controls.Add(this.btn_conn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txt_result);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btn_conn;
+        private System.Windows.Forms.TextBox txt_result;
+        private System.Windows.Forms.Button btn_check;
+    }
+}
+
---
+++ zz_SerialPort/SerialPort_01/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SerialPort_01
+{
+    public partial class Form1 : Form
+    {
+
+        private SerialPort serialPort = new SerialPort();
+        string receivedData = "";
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 設置串口參數
+            serialPort.PortName = "COM1";  // 更換為您的串口名稱
+            serialPort.BaudRate = 115200;    // 波特率
+            serialPort.DataBits = 8;        // 數據位
+            serialPort.Parity = Parity.None; // 奇偶校驗
+            serialPort.StopBits = StopBits.One; // 停止位
+
+            // 設置串口數據接收事件處理程序
+            //serialPort.DataReceived += SerialPort_DataReceived;
+
+            
+        }
+
+        private void btn_conn_Click(object sender, EventArgs e)
+        {
+            serialPort.Close();
+            Thread.Sleep(1000);
+
+            // 開啟串口（在需要的地方調用）
+            serialPort.Open();
+            Thread.Sleep(1000);
+
+            
+        }
+
+        private void SerialPort_DataReceived()
+        {
+            // 在這裡處理接收到的數據
+            receivedData = serialPort.ReadExisting();
+            // 可以將receivedData顯示在UI上，例如使用Invoke方法更新UI控件
+            // textBoxReceivedData.Invoke((MethodInvoker)delegate { textBoxReceivedData.Text += receivedData; });
+
+           
+        }
+
+        private void btn_check_Click(object sender, EventArgs e)
+        {
+            serialPort.Write("*IDN?" + "\r\n");
+            Thread.Sleep(1000);
+            SerialPort_DataReceived();
+            txt_result.Text += receivedData + "\r\n";
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+
+        }
+    }
+}
