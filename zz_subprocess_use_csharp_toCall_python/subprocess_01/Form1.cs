9043aa68142a8496836fd7191bd4408cdb07e125
YR <darkbuq@gmail.com>
2024-01-29 14:06:46 +0800

    20240129

---
+++ zz_subprocess_use_csharp_toCall_python/subprocess_01/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace subprocess_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (Process process = new Process())
+            {
+                process.StartInfo.FileName = "python";
+                process.StartInfo.Arguments = "test01.py";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.Start();
+
+                string output = process.StandardOutput.ReadToEnd();
+
+                //Console.WriteLine(output);
+                textBox1.Text = output;
+            }
+        }
+    }
+}
---
+++ zz_subprocess_use_csharp_toCall_python/test01.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 29 09:39:44 2024
+
+@author: yr.chen
+"""
+
+print("gg")
