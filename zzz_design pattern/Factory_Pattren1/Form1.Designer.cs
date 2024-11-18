e2ec4e19922cec29a3236a221e74dadfacdf842c
chen yan rong <darkbuq@gmail.com>
2024-11-18 12:04:11 +0800

    add Factory pattern

---
+++ zzz_design pattern/Factory_Pattren1/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace Factory_Pattren1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_result = new System.Windows.Forms.TextBox();
+            this.btn_ICinitialize = new System.Windows.Forms.Button();
+            this.btn_FWsystem = new System.Windows.Forms.Button();
+            this.btn_EEPROM = new System.Windows.Forms.Button();
+            this.btn_SQLcontrol = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_result
+            // 
+            this.txt_result.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_result.Location = new System.Drawing.Point(12, 61);
+            this.txt_result.Multiline = true;
+            this.txt_result.Name = "txt_result";
+            this.txt_result.Size = new System.Drawing.Size(306, 137);
+            this.txt_result.TabIndex = 5;
+            // 
+            // btn_ICinitialize
+            // 
+            this.btn_ICinitialize.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ICinitialize.Location = new System.Drawing.Point(220, 12);
+            this.btn_ICinitialize.Name = "btn_ICinitialize";
+            this.btn_ICinitialize.Size = new System.Drawing.Size(98, 33);
+            this.btn_ICinitialize.TabIndex = 2;
+            this.btn_ICinitialize.Text = "ICinitialize";
+            this.btn_ICinitialize.UseVisualStyleBackColor = true;
+            this.btn_ICinitialize.Click += new System.EventHandler(this.btn_ICinitialize_Click);
+            // 
+            // btn_FWsystem
+            // 
+            this.btn_FWsystem.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_FWsystem.Location = new System.Drawing.Point(116, 12);
+            this.btn_FWsystem.Name = "btn_FWsystem";
+            this.btn_FWsystem.Size = new System.Drawing.Size(98, 33);
+            this.btn_FWsystem.TabIndex = 3;
+            this.btn_FWsystem.Text = "FWsystem";
+            this.btn_FWsystem.UseVisualStyleBackColor = true;
+            this.btn_FWsystem.Click += new System.EventHandler(this.btn_FWsystem_Click);
+            // 
+            // btn_EEPROM
+            // 
+            this.btn_EEPROM.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_EEPROM.Location = new System.Drawing.Point(12, 12);
+            this.btn_EEPROM.Name = "btn_EEPROM";
+            this.btn_EEPROM.Size = new System.Drawing.Size(98, 33);
+            this.btn_EEPROM.TabIndex = 4;
+            this.btn_EEPROM.Text = "EEPROM";
+            this.btn_EEPROM.UseVisualStyleBackColor = true;
+            this.btn_EEPROM.Click += new System.EventHandler(this.btn_EEPROM_Click);
+            // 
+            // btn_SQLcontrol
+            // 
+            this.btn_SQLcontrol.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_SQLcontrol.Location = new System.Drawing.Point(12, 204);
+            this.btn_SQLcontrol.Name = "btn_SQLcontrol";
+            this.btn_SQLcontrol.Size = new System.Drawing.Size(98, 33);
+            this.btn_SQLcontrol.TabIndex = 4;
+            this.btn_SQLcontrol.Text = "SQL control";
+            this.btn_SQLcontrol.UseVisualStyleBackColor = true;
+            this.btn_SQLcontrol.Click += new System.EventHandler(this.btn_SQLcontrol_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 278);
+            this.Controls.Add(this.txt_result);
+            this.Controls.Add(this.btn_ICinitialize);
+            this.Controls.Add(this.btn_FWsystem);
+            this.Controls.Add(this.btn_SQLcontrol);
+            this.Controls.Add(this.btn_EEPROM);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_result;
+        private System.Windows.Forms.Button btn_ICinitialize;
+        private System.Windows.Forms.Button btn_FWsystem;
+        private System.Windows.Forms.Button btn_EEPROM;
+        private System.Windows.Forms.Button btn_SQLcontrol;
+    }
+}
+
---
+++ zzz_design pattern/Factory_Pattren1/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Factory_Pattren1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_EEPROM_Click(object sender, EventArgs e)
+        {
+            Worktype wt = OperationFactory.Creatworktype("EEPROM");
+            txt_result.Text = wt.Workresult();
+        }
+
+        private void btn_FWsystem_Click(object sender, EventArgs e)
+        {
+            Worktype wt = OperationFactory.Creatworktype("FWsystem");
+            txt_result.Text = wt.Workresult();
+        }
+
+        private void btn_ICinitialize_Click(object sender, EventArgs e)
+        {
+            Worktype wt = OperationFactory.Creatworktype("ICinitialize");
+            txt_result.Text = wt.Workresult();
+        }
+
+        private void btn_SQLcontrol_Click(object sender, EventArgs e)
+        {
+            Worktype wt = OperationFactory.Creatworktype("SQLcontrol");
+            txt_result.Text = wt.Workresult();
+        }
+    }
+
+    public abstract class Worktype
+    {
+        public virtual string Workresult()
+        {
+            return "";
+        }
+
+
+    }
+
+    #region -- worktype class --
+    public class EEPROM : Worktype
+    {
+        public override string Workresult()
+        {
+            return "先檢查密碼是不是不能用\r\n再確認其他屎尿";
+        }
+    }
+
+    public class FWsystem : Worktype
+    {
+        public override string Workresult()
+        {
+            return "驗証的找SW\r\n正式發行找Mila";
+        }
+    }
+
+    public class ICinitialize : Worktype
+    {
+        public override string Workresult()
+        {
+            return "FW工程師要提交正式文件\r\n現在已導入BOM表系統\r\n所以要找Shelly & SW";
+        }
+    }
+
+    public class SQLcontrol : Worktype
+    {
+        public override string Workresult()
+        {
+            return "產線無法處理 或 需要確認的操作\r\n由SW確認後 直接操作SQL";
+        }
+    }
+    #endregion
+
+
+    public interface IFactory
+    {
+        Worktype Creatworktype(string worktype);
+    }
+
+    public class FactoryBasic: IFactory
+    {
+        public Worktype Creatworktype(string worktype)
+        {
+            Worktype wt = null;
+
+            if (worktype == "EEPROM")
+            {
+                wt = new EEPROM();
+            }
+            else if (worktype == "FWsystem")
+            {
+                wt = new FWsystem();
+            }
+            else if (worktype == "ICinitialize")
+            {
+                wt = new ICinitialize();
+            }
+
+            return wt;
+        }
+    }
+
+    public class FactoryAdvanced : IFactory
+    {
+        public Worktype Creatworktype(string worktype)
+        {
+            Worktype wt = null;
+
+            if (worktype == "SQLcontrol")
+            {
+                wt = new SQLcontrol();
+            }
+            
+
+            return wt;
+        }
+    }
+
+    public class OperationFactory
+    {
+        public static Worktype Creatworktype(string worktype)
+        {
+            IFactory factory = null;
+
+            switch (worktype)
+            {
+                case "EEPROM":
+                case "FWsystem":
+                case "ICinitialize":
+                    factory = new FactoryBasic();
+                    break;
+                case "SQLcontrol":
+                    factory = new FactoryAdvanced();
+                    break;
+            }
+
+            Worktype wt = null;
+            wt = factory.Creatworktype(worktype);
+
+            return wt;
+        }
+    }
+}
